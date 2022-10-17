99e5f4e6aa3cc41ab780958586e9b5dfef697c1a
FaridKaveh <farid.kaveh20@imperial.ac.uk>
2022-10-17 10:51:47 +0100

    added agent header file

---
+++ agent.h
@@ -0,0 +1,17 @@
+#pragma once 
+#include <vector> 
+
+class Agent { 
+	private: 
+		double delta, epsilon; 
+		double min_comp, max_comp;
+	       	std::vector<double> comp_arr; 
+		std::vector<double> comp_dens_arr;
+	
+	public: 
+		Agent(); //default constructor defined in implementation
+		
+		void get_params(){printf("delta: %f, epsilon: %f, comp range: [%f, %f]", delta, epsilon, min_comp,max_comp); }	
+	
+};
+
--- stoch_engine.cpp
+++ stoch_engine.cpp
@@ -1,33 +1,22 @@
 #include <iostream> 
 #include <vector> 
 #include <algorithm> 
+#include "agent.h"
 
+Agent::Agent(){ 
+	// default constructor for agent. Sets the competence range to [-3,3]
+	delta = 0.1; 
+	epsilon = delta;
+	max_comp = 3; min_comp = -3; 
+	double intervals = (max_comp-min_comp)/epsilon;	
+	for (int i = 0; i < intervals; i++) comp_arr.push_back(max_comp-i*epsilon); 
+			
 
-class Agent{
-	/* The Agent class defines a stochastic engine which plays a game where there is an optimal 
-    or 'confident' strategy. The skill rating of the agent is updated using a Bayesian model
-    whenever the it makes a move using the 'update_dens' function  */
-
-	private: 
-		float delta, epsilon; 
-		float min_comp, max_comp;
-	       	std::vector<float> comp_arr; 
-		std::vector<float> comp_dens_arr; 
-	public:
-		Agent (){ 
-			// default constructor for agent. Sets the competence range to [-3,3]
-			delta = 0.1; 
-			epsilon = delta;
-			max_comp = 3; min_comp = -3; 
-			float intervals = (max_comp-min_comp)/epsilon;	
-			for (int i = 0; i < intervals; i++){
-				comp_arr.push_back(max_comp-i*epsilon); 
-			}
-
-			comp_dens_arr.assign(comp_arr.size(), 1/comp_arr.size()); 
+	comp_dens_arr.assign(comp_arr.size(), 1/comp_arr.size()); 
 		}	
-};
 
 int main() { 
-	return 0;
-}
+	Agent player1; 
+	player1.get_params(); 
+	return 0; 
+} 
