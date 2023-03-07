8cd090e3c5ee7b4c32783b109d1b82b072acefd1
FaridKaveh <farid.kaveh20@imperial.ac.uk>
2023-03-07 15:49:04 +0000

    added stockfish_dialogue.cpp

---
+++ cpp_implementation/stockfish_dialogue.cpp
@@ -0,0 +1,8 @@
+#include <string>
+
+
+/* To get move evaluations from stockfish, dikastis needs to have a dialogue with stockfish. This file handles
+their dialogue by sending commands from dikastis and receiving responses from stockfish */
+
+
+
