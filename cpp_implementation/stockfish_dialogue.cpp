dcd4e161c73c666989fe09ad67c81d20fc112f55
FaridKaveh <farid.kaveh20@imperial.ac.uk>
2023-03-07 16:09:08 +0000

    na

--- cpp_implementation/stockfish_dialogue.cpp
+++ cpp_implementation/stockfish_dialogue.cpp
@@ -1,8 +1,27 @@
+#include <iostream>
 #include <string>
+#include <fstream>
 
 
 /* To get move evaluations from stockfish, dikastis needs to have a dialogue with stockfish. This file handles
 their dialogue by sending commands from dikastis and receiving responses from stockfish */
 
 
+std::string 
+get_fen(const char* file){ 
+    std::ifstream ifin(file); 
+    std::string fen; 
+    std::getline(ifin, fen);
+    
+    ifin.close();
+    std::cout << fen.c_str() << std::endl;
+    return fen; 
+    
+} 
 
+int main() {
+    const char* file = "moves.txt";
+    get_fen(file);
+
+    return 0; 
+}
