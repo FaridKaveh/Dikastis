a07ea40b04d0de4e7a7990d6fc245a99ea1593ed
FaridKaveh <farid.kaveh20@imperial.ac.uk>
2023-03-11 21:09:44 +0000

    added read_from_file function

--- cpp_implementation/helper.cpp
+++ cpp_implementation/helper.cpp
@@ -1,7 +1,11 @@
 #include <vector> 
-#include "math.h"
 
-std::vector<double> populate_vector(double start, double inc, int n){ 
+/* A file for helper function implmentation.*/
+
+std::vector<double> 
+populate_vector(double start, double inc, int n){ 
+
+    /* Returns a vector v populated by {start - inc, start - 2* inc, ... , start - n*inc}*/
 	std::vector<double> v; 
 	v.reserve(n-1);	
 	for (int i = 1; i < n; i++){ 
@@ -9,5 +13,19 @@ std::vector<double> populate_vector(double start, double inc, int n){
     }
 
     return v; 
+}
+
+void 
+read_from_file(int file){ 
+    /* reads the contents of a file stream and prints to stdout*/
+
+
+    FILE *stream; //the FILE struct identifies a stream and info needed to control it
+    stream = fdopen(file, "r"); 
+    int c; //fgetc returns an unsigned char cast to an int
+    while ((c= fgetc(stream) )!= EOF) { 
+        putchar (c); 
+    }
+    
 }
 
