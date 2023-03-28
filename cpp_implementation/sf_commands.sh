e66e716bc8d420c1341ccd85cf64edaee625d2bb
FaridKaveh <farid.kaveh20@imperial.ac.uk>
2023-03-28 13:41:37 +0100

    bash script changes

--- cpp_implementation/sf_commands.sh
+++ cpp_implementation/sf_commands.sh
@@ -3,7 +3,7 @@ spawn /home/farid/Apps/stockfish_15_linux_x64_avx2/stockfish15
 expect Stockfish
 send "position fen rnb1kbnr/ppp1pppp/8/8/1q6/2N5/P1PP1PPP/1RBQKBNR b Kkq - 1 5 \n isready \n"
 expect readyok
-send "go depth 15 \n " 
-expect bestmove 
+send "eval \n " 
+expect Final 
 send " isready \n"
 expect readyok 
